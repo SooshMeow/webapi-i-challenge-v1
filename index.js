410f63b18b51a743c994d7ac37eaf5c861374798
unknown <robertrey1234@gmail.com>
2019-08-05 15:58:55 -0500

    Created server and one get request

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require('express');
+
+const Users = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(() => { 
+        res.status(500).json({ message: 'User info could not be retrieved'})  
+    })
+})
+
+const port = 8000;
+server.listen(port, () => console.log("Server Running"))
